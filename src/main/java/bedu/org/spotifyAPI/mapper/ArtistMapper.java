b8334ce0cdb2145b590c70684828f4331146127d
Emilio Luna <al244182@edu.uaa.mx>
2023-12-04 20:51:30 -0600

    Mapper

--- src/main/java/bedu/org/spotifyAPI/mapper/ArtistMapper.java
+++ src/main/java/bedu/org/spotifyAPI/mapper/ArtistMapper.java
@@ -10,7 +10,8 @@ import bedu.org.spotifyAPI.model.Artist;
 @Mapper(componentModel = "spring",injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface ArtistMapper {
 
-   
+  
+   ArtistDTO toDTO(Artist model);
    List <ArtistDTO> toDTO(List <Artist> model);
     
 }
